a64ad3164bab79791f9728779a6918723be7c56a
Kirill13311 <62827666+KirillEL@users.noreply.github.com>
2023-10-03 18:39:48 +0700

    initial1

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello world dawljdwa
         ttt
+        <div className={'bg-amber-300 w-2 h-2'}>DUCK</div>
     </main>
   )
 }
