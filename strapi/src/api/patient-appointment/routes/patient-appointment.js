17da38903b40451a73671dc7623908e993112150
Kirill13311 <62827666+KirillEL@users.noreply.github.com>
2023-10-06 14:12:53 +0700

    update docker-compose v2

---
+++ strapi/src/api/patient-appointment/routes/patient-appointment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * patient-appointment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::patient-appointment.patient-appointment');
