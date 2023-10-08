7dc903864396ad8ca5b9681bbd618cffaca2da30
Kirill13311 <62827666+KirillEL@users.noreply.github.com>
2023-10-08 10:00:43 +0700

    change watch config for nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+  })
+}
 
 module.exports = nextConfig
