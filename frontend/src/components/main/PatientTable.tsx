3f23eb85d895c93cccbfd6315fe302a48d21c364
Kirill13311 <62827666+KirillEL@users.noreply.github.com>
2023-10-10 16:14:32 +0700

    new

--- frontend/src/components/main/PatientTable.tsx
+++ frontend/src/components/main/PatientTable.tsx
@@ -2,25 +2,40 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {useSession} from "next-auth/react";
-import jwtDecode from "jwt-decode";
+import jwtDecode from 'jwt-decode';
+
+interface Patient {
+  id: number;
+  attributes: Record<string, any>;
+}
 
 export default function PatientTable() {
   const {data: session} = useSession();
 
-  const [data, setData] = useState();
-  useEffect(() => {
-    axios.get("http://elertk133.fvds.ru:1337/api/patients", {headers: {Authorization: 'Bearer ' + session?.accessToken}})
-        .then(res => setData(res.data));
-  const decoded = jwtDecode(session?.accessToken)
-  console.log('decoded jwt =  ' + JSON.stringify(decoded));
-  }, [])
-console.log(data)
+  const [data, setData] = useState<Patient[] | null>(null);
 
-  const patientsList = data?.data.map(item => <li key={item.id} className={"m-4"}>{JSON.stringify(item.attributes)}</li>)
+  useEffect(() => {
+    if(session?.accessToken) {
+      axios.get<Patient[]>("http://elertk133.fvds.ru:1337/api/patients", {
+        headers: {Authorization: "Bearer " + session.accessToken},
+      })
+        .then(res => setData(res.data))
+        .catch(err => {console.error(err);});
+    }
+    const decoded = jwtDecode(session?.accessToken);
+    console.log("decoded jwt: " + JSON.stringify(decoded));
+
+  }, [session]);
+
+  const patientsList = data?.map((item) => (
+    <li key={item.id} className="m-4">
+      {JSON.stringify(item.attributes)}
+    </li>
+  ));
+
   return (
     <div className="m-5 p-1">
       <ul>{patientsList}</ul>
-
     </div>
   )
 }
