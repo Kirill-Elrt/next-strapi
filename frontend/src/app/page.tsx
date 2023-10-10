691d5de8c41c59cda403ba0fd32d7607a9ecd213
AxTerium <kirill.kazancev.2001@mail.ru>
2023-10-10 13:52:09 +0700

    jwt authorisation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,24 @@
 'use client';
 
-import {signOut, useSession} from 'next-auth/react';
+import {useSession} from 'next-auth/react';
+import PatientTable from "@/components/main/PatientTable";
 
 export default function HomePage() {
   const { data: session, status } = useSession();
   console.log(session);
 
+
     return <div className={"border-4 w-screen h-screen bg-teal-50"}> {status == "loading"?
         (<div>Loading...</div>) :
-        (<div>
-          <p className={"m-2 p-2"}>
-            Hello {session?.user?.email}
-          </p>
-          <button onClick={() => signOut()} className={"border border-black rounded-lg p-2 m-2 bg-neutral-300 hover:bg-neutral-400"}>
-            Выход
-          </button>
+        (<div className={"grid grid-cols-2"}>
+          <div>
+            <button className={"m-2 p-2 w-60 border rounded-lg bg-teal-300 hover:bg-teal-400"}>Новый приём</button>
+            <button className={"m-2 p-2 w-60 border rounded-lg bg-teal-300 hover:bg-teal-400"}>Новый пациент</button>
+          </div>
+          <div>Фильтры</div>
+          <div>
+            <PatientTable/>
+          </div>
         </div>)}
     </div>;
 }
